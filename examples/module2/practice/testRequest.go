f0e6407756ba783ae3c9944cce5280c5ba2f2f78
shibo <bo.shi@yintech.cn>
2022-07-24 16:15:15 +0800

    practice

--- examples/module2/practice/testRequest.go
+++ examples/module2/practice/testRequest.go
@@ -12,15 +12,13 @@ func testRequest(w http.ResponseWriter, r *http.Request) {
 	version := os.Getenv("VERSION")
 	w.Header().Set("VERSION", version)
 	fmt.Printf("os version: %s \n", version)
-	// 02.将requst中的header 设置到 reponse中
 	for k, v := range r.Header {
 		for _, vv := range v {
 			fmt.Printf("Header key: %s, Header value: %s \n", k, v)
 			w.Header().Set(k, vv)
 		}
 	}
-	// 04.记录日志并输出
-	clientip := getClientIP(r)
+	clientIp := getClientIP(r)
 	log.Printf("Success! Response code: %d", 200)
-	log.Printf("Success! clientip: %s", clientip)
+	log.Printf("Success! clientIp: %s", clientIp)
 }
